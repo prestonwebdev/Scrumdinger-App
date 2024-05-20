65440f8039ebc456264559ae7b6d505331cbfafa
Preston <preston.t.booth@gmail.com>
2024-05-20 16:41:15 -0600

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Preston Booth on 5/20/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
