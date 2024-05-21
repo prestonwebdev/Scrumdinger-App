23ebdc5f10b48d5c26ceeffb34ec7ba543b50fc5
Preston <preston.t.booth@gmail.com>
2024-05-20 19:25:12 -0600

    Lesson 1

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -9,15 +9,45 @@ import SwiftUI
 
 struct MeetingView: View {
     var body: some View {
-        VStack(spacing: 12.0) {
-            Text("👋")
-                .font(.largeTitle)
-            Text("I am a developer now!")
+        VStack(spacing: 0.0) {
+            ProgressView(value:2, total:15)
+            HStack() {
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed")
+                        .font(.caption)
+                    Label("300", systemImage: "hourglass.tophalf.fill")
+                }
+                
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining")
+                        .font(.caption)
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Time Remaining")
+            .accessibilityValue("10 minutes")
+            Circle()
+                .strokeBorder(lineWidth: 24)
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {}) {
+                    Image(systemName: "forward.fill")
+                }
+                .accessibilityLabel("Next Speaker")
+            }
+            
         }
+        
         .padding()
+        
     }
 }
 
-#Preview {
-    MeetingView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
 }
